29900085a5d864fa2f816d0d248b6678d924aca9
mnqural <71308414+mnqural@users.noreply.github.com>
2020-09-15 10:14:03 +0500

    Create lab0.cs

---
+++ lab0.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace _2020
+{
+    class lab0
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Вы ввели: {0}", name);
+
+            Console.ReadKey();
+        }
+    }
+}
